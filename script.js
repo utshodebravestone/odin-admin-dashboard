f27dc2a764dffb8067b92d27cfad4b00ee054afe
utsho <utshodebravestone@gmail.com>
2023-08-26 20:53:04 +0600

    Added header

---
+++ script.js
@@ -0,0 +1,6 @@
+if (window.innerWidth < 1250) {
+  console.log(window.innerWidth);
+  alert(
+    "Your window is too small to use this app properly. You could perhaps enable your device's Landscape Mode too see and use the app properly."
+  );
+}
